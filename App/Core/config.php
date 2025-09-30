fc367a785d43700bc1c4b6a785742ef344c3fae3
Sameer Ali <sameeralifazal@gmail.com>
2025-09-30 06:21:16 +0500

    Changed the JWT key testing

--- App/Core/config.php
+++ App/Core/config.php
@@ -4,5 +4,5 @@ const HOST = 'localhost';
 const DB_NAME = 'blog';
 const USER_NAME = 'root';
 const DB_PASSWORD = '';
-const JWT_KEY = 'Thisisasecretkeywhichiwillnottellanyonealsomayigotoheaven';
+const JWT_KEY = 'testSecretKey';
 ?>
